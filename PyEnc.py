053c8efb74de64eb4b35b3a85a15696c9a18b690
ToxicNoob <ToxicNoob.Sl4d3.Official@gmail.com>
2023-01-01 23:58:25 +0600

    Update PyEnc.py

--- PyEnc.py
+++ PyEnc.py
@@ -19,7 +19,7 @@ try:
 	columns = shutil.get_terminal_size().columns
 	column1 = columns + 15
 	column2 = columns + 5
-	column3 = columns + 20
+	column3 = columns + 19
 except:
 	rows, columns = os.popen('stty size', 'r').read().split()
 	column1 = 0
@@ -35,10 +35,10 @@ def logo():
     print("\033[94m│    \033[92m▌  ▚▄▌▌  ▌ ▌▌ ▖▌  ▚▄▌▙▄▘▐ ▖▌ ▌▌  \033[94m   │".center(column1))
     print("\033[94m│    \033[92m▘  ▗▄▘▀▀▘▘ ▘▝▀ ▘  ▗▄▘▌   ▀ ▝▀ ▘  \033[94m   │".center(column1))
     print("\033[94m│                                        │".center(column2))
-    print("\033[94m│ \033[95mAuthor : ToxicNoob                     \033[94m│".center(column1))
+    print("\033[94m│ \033[95mAuthor : ToxicNoob Inc.                \033[94m│".center(column1))
     print("\033[94m│ \033[95mTool   : Encode Python2 and Python3    \033[94m│".center(column1))
     print("\033[94m│ \033[95mGitHub : https://github.com/Toxic-Noob \033[94m│".center(column1))
-    print("\033[94m│ \033[95mCoder  : HunterSl4d3              \033[37mV2.0 \033[94m│".center(column3))
+    print("\033[94m│ \033[95mCoder  : HunterSl4d3              \033[37mV3.0 \033[94m│".center(column3))
     print("\033[94m└────────────────────────────────────────┘".center(column2))
 
 #Exit Tool Massage
@@ -75,49 +75,56 @@ def verInput(data):
         dataInput = input(data)
     return dataInput
 
+
 #Marshal_Code_Executor#
-marshalHead = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport marshal\n\nexec(marshal.loads("
+marshalHead = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport marshal\nexec(marshal.loads("
 
 marshalTail = "))"
 
 #Base64_Code_Executor#
-b64Head = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport base64\n\nexec(base64.b64decode("
+b64Head = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport base64\nexec(base64.b64decode("
 
 b64Tail = "))"
 
 #Zlib_Code_Executor#
-zlibHead = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport zlib\n\nexec(zlib.decompress("
+zlibHead = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport zlib\nexec(zlib.decompress("
 
 zlibTail = "))"
 
 #Zlib+B64+Mar_Code_Executor#
-allHead = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport marshal, base64, zlib\n\nexec(marshal.loads(zlib.decompress(base64.b64decode("
+allHead = "# Encoded By PyEncryptor\n# A Product Of ToxicNoob\n# https://github.com/Toxic-Noob\n\nimport marshal, base64, zlib\nexec(marshal.loads(zlib.decompress(base64.b64decode("
 
 allTail = "))))"
 
 #Encode Marshal
-def encodeMarshal(data):
-    code = compile(data, "ToxicNoob", 'exec')
-    dump = marshal.dumps(code)
-    encData = marshalHead +repr(dump) + marshalTail
+def encodeMarshal(data, power):
+    powerData = data
+    for i in range(power):
+        code = compile(powerData, "ToxicNoob", "exec")
+        dump = marshal.dumps(code)
+        powerData = marshalHead +repr(dump) + marshalTail
     
-    return encData
+    return powerData
 
 #Encode Zlib
-def encodeZlib(data):
-    code = data.encode()
-    dump = zlib.compress(code, 2)
-    encData = zlibHead + repr(dump) + zlibTail
+def encodeZlib(data, power):
+    powerData = data
+    for i in range(power):
+        code = powerData.encode()
+        dump = zlib.compress(code, 2)
+        powerData = zlibHead + repr(dump) + zlibTail
     
-    return encData
+    return powerData
 
 #Encode Base64
-def encodeBase64(data):
-    code = data.encode()
-    dump = base64.b64encode(code)
-    encData = b64Head + repr(dump) + b64Tail
+def encodeBase64(data, power):
+    powerData = data
+    for i in range(power):
+        code = powerData.encode()
+        dump = base64.b64encode(code)
+        powerData = b64Head + repr(dump) + b64Tail
     
-    return encData
+    return powerData
 
 #Python Bytecode Encode
 def encodePyc(data):
@@ -132,51 +139,51 @@ def encodePyc(data):
     return True
 
 #Marshal + Zlib + Base64
-def encodeAll(data):
-    data = compile(data, "ToxicNoob", "exec")
-    data = marshal.dumps(data)
-    data = zlib.compress(data)
-    dump = base64.b64encode(data)
-    encData = allHead + repr(dump) + allTail
+def encodeAllOnce(data, power):
+    powerData = data
+    for i in range(power):
+        code = compile(powerData, "ToxicNoob", "exec")
+        code = marshal.dumps(code)
+        code = zlib.compress(code)
+        dump = base64.b64encode(code)
+        powerData = allHead + repr(dump) + allTail
     
-    return encData
+    return powerData
 
-#Hard Encode
-def encodeHard(data):
-    power = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Hard Power Amount (\033[37mMAX: 15\033[92m):> \033[37m")
+# Marshal + Zlib + Base64 with One By One
+def encodeAllStep(data, power):
+    powerData = data
+    for i in range(power):
+        code = encodeBase64(powerData, power=1)
+        code = encodeZlib(code, power=1)
+        powerData = encodeMarshal(code, power=1)
     
-    try:
-        power = int(power)
-    except:
-        psb("\n\033[92m    [\033[91m*\033[92m] Your Power Amount Must Be a Number!")
-        power = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Hard Power Amount (\033[37mMAX: 15\033[92m):> \033[37m")
-        power = int(power)
+    return powerData
+
+
+# Get power amount
+def getPower():
+    power = verInput("\033[92m    [\033[37m*\033[92m]\033[37m Enter Repeat Amount (\033[92mMAX: 15\033[37m):> \033[92m")
+    while not power.isdigit():
+        psb("\n\033[92m    [\033[91m!\033[92m]\033[37m Enter a Correct Amount!")
+        power = verInput("\033[92m    [\033[37m*\033[92m]\033[37m Enter Repeat Amount (\033[92mMAX: 15\033[37m):> \033[92m")
+    
+    power = int(power)
     
     while (power > 15):
-        psb("\n\033[92m    [\033[91m*\033[92m] Max Hard Amount Is \033[37m15!")
-        power = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Hard Power Amount (\033[37mMAX: 15\033[92m):> \033[37m")
-        power = int(power)
+        psb("\n\033[92m    [\033[91m!\033[92m]\033[37m Maximum amount is 15")
+        power = getPower()
     
-    psb("\n\033[92m    [\033[37m*\033[92m] Encoding Your File...\033[30m")
-    
-    powerData = data
-    
-    for i in range(power):
-        powerData = encodeAll(powerData)
-    
-    encodePyc(powerData)
-    
-    return True
-
+    return int(power)
 
 #Get File Data
 def getFile():
-    path = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Your File Path:> \033[37m")
+    path = verInput("\n\033[92m    [\033[37m*\033[92m]\033[37m Enter Your File Path:> \033[92m")
     
     while not os.path.exists(path):
-        psb("\n\033[92m    [\033[91m!\033[92m] File Does Not Exists!")
+        psb("\n\033[92m    [\033[91m!\033[92m]\033[37m File Does Not Exists!")
         time.sleep(0.4)
-        path = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Your File Path:> \033[37m")
+        path = verInput("\033[92m    [\033[37m*\033[92m]\033[37m Enter Your File Path:> \033[92m")
     
     fileData = open(path, "r").read()
     
@@ -209,55 +216,54 @@ def moveFile():
         savefileName = fileName + "_enc.py"
     
     savePath = "/sdcard/PyEncryptor/" + savefileName
-    
     os.system("mv temp.pyc " + savePath + " >/dev/null 2>&1")
     os.system("rm temp.py > /dev/null 2>&1")
-    
 
 
 #Encoding Process
 def encode(type):
     fileData = getFile()
     
+    if not (type == "pyc"):
+        power = getPower()
     
-    if not (type == "hard"):
-        psb("\n\033[92m    [\033[37m*\033[92m] Encoding Your File...\033[30m")
+    psb("\n\033[92m    [\033[37m*\033[92m]\033[37m Encoding Your File...\033[30m")
     
     if (type == "marshal"):
-        encData = encodeMarshal(fileData)
+        encData = encodeMarshal(fileData, power)
         saveFile(encData)
         
     elif (type == "zlib"):
-        encData = encodeZlib(fileData)
+        encData = encodeZlib(fileData, power)
         saveFile(encData)
         
     elif (type == "base64"):
-        encData = encodeBase64(fileData)
+        encData = encodeBase64(fileData, power)
         saveFile(encData)
         
     elif (type == "pyc"):
         encData = encodePyc(fileData)
         moveFile()
         
-    elif (type == "all"):
-        encData = encodeAll(fileData)
+    elif (type == "all_once"):
+        encData = encodeAllOnce(fileData, power)
         saveFile(encData)
-        
-    elif (type == "hard"):
-        encData = encodeHard(fileData)
-        moveFile()
+    
+    elif (type == "all_step"):
+        encData = encodeAllStep(fileData, power)
+        saveFile(encData)
     
     
     time.sleep(1)
-    psb("\n\033[92m    [\033[37m*\033[92m] Encoding Complete!")
-    psb("\n\033[92m    [\033[37m*\033[92m] File Saved In \033[37m/sdcard/PyEncryptor \033[92m Directory, as \033[37m " + fileName.replace(".py", "_enc.py") + "\033[92m....\033[37m\n")
+    psb("\n\033[92m    [\033[37m*\033[92m]\033[37m Encoding Complete!")
+    psb("\033[92m    [\033[37m*\033[92m]\033[37m Saved File Path: \033[92m/sdcard/PyEncryptor/" + fileName.replace(".py", "_enc.py") + " \033[37m\n")
 
 
 #Update Tool
 def update():
     logo()
-    psb("\n\033[92m    [\033[37m*\033[92m] Please Wait!")
-    psb("\033[92m    [\033[37m*\033[92m] Checking Update...")
+    psb("\n\033[92m    [\033[37m*\033[92m]\033[37m Please Wait!")
+    psb("\033[92m    [\033[37m*\033[92m]\033[37m Checking Update...")
     import requests
     try:
         toolVersion = open(".version", "r").read()
@@ -266,20 +272,21 @@ def update():
     try:
         mainVersion = requests.get("https://raw.githubusercontent.com/Toxic-Noob/PyEncryptor/main/.version").text
     except:
-        psb("\n\033[92m    [\033[91m!\033[92m] Please Turn On Internet Connection!")
-        l = verInput("\n\033[92m    [\033[37m*\033[92m] Press Enter To Continue....")
+        psb("\n\033[92m    [\033[91m!\033[92m]\033[37m Please Turn On Internet Connection!")
+        l = verInput("\n\033[92m    [\033[37m*\033[92m]\033[37m Press Enter To Continue....")
         update()
     if (toolVersion == mainVersion):
-        psb("\n\033[92m    [\033[37m*\033[92m] You are Using the Latest Version of PyEncryptor!")
-        l = verInput("\033[92m    [\033[37m*\033[92m] Press Enter To Go Back....")
+        psb("\n\033[92m    [\033[37m*\033[92m]\033[37m You are Using the Latest Version of PyEncryptor!")
+        l = verInput("\033[92m    [\033[37m*\033[92m]\033[37m Press Enter To Go Back....")
         logo()
         main()
     else:
-        psb("\n\033[92m    [\033[37m!\033[92m] Tool Update Found!!")
-        psb("\033[92m    [\033[37m*\033[92m] Updating Tool...")
+        psb("\n\033[92m    [\033[37m*\033[92m]\033[37m Tool Update Found!!")
+        psb("\033[92m    [\033[37m*\033[92m]\033[37m Updating Tool...")
         os.system("cd .. && rm -rf PyEncryptor && git clone https://github.com/Toxic-Noob/PyEncryptor > /dev/null 2>&1")
-        psb("\n\033[92m    [\033[37m*\033[92m] Tool Updated Successfully!")
-        psb("\033[92m    [\033[37m*\033[92m] Starting Latest Version of PyEncryptor...")
+        
+        psb("\n\033[92m    [\033[37m*\033[92m]\033[37m Tool Updated Successfully!")
+        psb("\033[92m    [\033[37m*\033[92m]\033[37m Starting Latest Version of PyEncryptor...")
         time.sleep(1)
         os.system("cd .. && cd PyEncryptor && python PyEnc.py")
 
@@ -287,22 +294,22 @@ def update():
 
 #Main Menu
 def main():
-    psb("\n\033[92m    [\033[37m*\033[92m] Choose Your Option:")
+    psb("\n\033[92m    [\033[37m*\033[92m]\033[37m  Choose Your Option:")
     
-    print("\n\033[92m    [\033[37m01\033[92m] Marshal")
-    print("\033[92m    [\033[37m02\033[92m] Zlib")
-    print("\033[92m    [\033[37m03\033[92m] Base64")
-    print("\033[92m    [\033[37m04\033[92m] Python Bytecode (\033[37m.pyc\033[92m)")
-    print("\033[92m    [\033[37m05\033[92m] Marshal + Zlib + Base64")
-    print("\033[92m    [\033[37m06\033[92m] Hard Encrypt")
-    print("\033[92m    [\033[37m07\033[92m] Update Tool")
-    print("\033[92m    [\033[37m08\033[92m] Exit")
+    print("\n\033[92m    [\033[37m01\033[92m]\033[37m Marshal")
+    print("\033[92m    [\033[37m02\033[92m]\033[37m Zlib")
+    print("\033[92m    [\033[37m03\033[92m]\033[37m Base64")
+    print("\033[92m    [\033[37m04\033[92m]\033[37m Python Bytecode (\033[92m.pyc\033[37m)")
+    print("\033[92m    [\033[37m05\033[92m]\033[37m Marshal + Zlib + Base64 (\033[92mAt Once\033[37m)")
+    print("\033[92m    [\033[37m06\033[92m]\033[37m Marshal + Zlib + Base64 (\033[92mOne By One\033[37m)")
+    print("\033[92m    [\033[37m07\033[92m]\033[37m Update Tool")
+    print("\033[92m    [\033[37m08\033[92m]\033[37m Exit")
     
-    op = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Your Choice:> \033[37m").replace("0", "")
+    op = verInput("\n\033[92m    [\033[37m*\033[92m]\033[37m Enter Your Choice:> \033[92m").replace("0", "")
     
     while not (op in ["1", "2", "3", "4", "5", "6", "7", "8"]):
-        psb("\n\033[92m    [\033[91m!\033[92m] Please Enter a Correct Option!")
-        op = verInput("\n\033[92m    [\033[37m*\033[92m] Enter Your Choice:> \033[37m").replace("0", "")
+        psb("\n\033[92m    [\033[91m!\033[92m]\033[37m Please Enter a Correct Option!")
+        op = verInput("\033[92m    [\033[37m*\033[92m]\033[37m Enter Your Choice:> \033[92m").replace("0", "")
     
     if (op == "1"):
         encode("marshal")
@@ -313,9 +320,9 @@ def main():
     elif (op == "4"):
         encode("pyc")
     elif (op == "5"):
-        encode("all")
+        encode("all_once")
     elif (op == "6"):
-        encode("hard")
+        encode("all_step")
     elif (op == "7"):
         update()
     elif (op == "8"):
